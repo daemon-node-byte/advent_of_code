8e044099d3b39fa3c679073948ab6a80019d4de2
Josh McLain <joshmclain45@gmail.com>
2021-05-07 01:12:39 -0700

    updating day 1 of 2020

---
+++ 2020/README.md
@@ -0,0 +1,11 @@
+# adventOfCode 2020
+
+## Day 1
+
+### Part 1
+
+this one is pretty straight forward. the goal is find two numbers that total 2020, and return the product of the two numbers that sum 2020 from the provided input for part one I simple used two for loops to find the numbers that summed 2020 and return the product of those two nubers
+
+### Part 2
+
+same thing, self explanitory, but this time we are looking for 3 numbers that sum 2020,  then return the product of those 3 numbers
---
+++ 2020/day1Solution.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const DATA = fs
+  .readFileSync("./inputDayOne", "utf-8")
+  .split("\n")
+  .map((val) => +val);
+
+// console.log(DATA)
+// Part One
+function sumTwoFindProductOf(data) {
+  for (let num1 of data) {
+    for (let num2 of data) {
+      if (num1 + num2 == 2020) return num1 * num2;
+    }
+  }
+  return "error";
+}
+
+// console.log(sumTwoFindProductOf(DATA))
+
+//Part Two
+function sumThreeFindProduct(data) {
+  for (let num1 of data) {
+    for (let num2 of data) {
+      for (let num3 of data) {
+        if (num1 + num2 + num3 == 2020) return num1 * num2 * num3;
+      }
+    }
+  }
+  return "error";
+}
+
+console.log(sumThreeFindProduct(DATA));
